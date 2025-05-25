d7eec4d13555da07d2dda45cd39ccb66f91a7ea4
Xebeth <github@north-edge.com>
2025-05-25 02:32:06 +0100

    Initial commit

---
+++ GenericModTranslator/Program.cs
@@ -0,0 +1,21 @@
+﻿using MagicLoaderGenerator.Localization.Providers;
+using MagicLoaderGenerator.Filesystem.Generators;
+using Microsoft.Extensions.Configuration;
+using MagicLoaderGenerator.Filesystem;
+using MagicLoaderGenerator;
+
+var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("config.json", optional: false);
+var appConfig = new AppConfig(builder.Build());
+var localization = new JsonLocalizationProvider(appConfig);
+var mod = new MagicLoaderMod(localization, appConfig);
+
+var outputDir = mod.Generate(new ZipOutputGenerator(appConfig));
+
+#if DEBUG
+System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo {
+    UseShellExecute = true,
+    FileName = outputDir,
+    Verb = "open"
+});
+#endif
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Description
+
+A generic mod based on the [MagicLoaderGenerator](https://github.com/North-Edge/MagicLoaderGenerator) library.\
+It will automatically translate any MagicLoader files placed in its `Input` directory and generate an archive per language.
